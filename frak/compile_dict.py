af9bd516f3984b9d2c0b7c70c66624c1f3f7a786
Brendan Myers <bcm3223@gmail.com>
2017-07-16 22:15:06 -0400

    Card List updated for set two

--- frak/compile_dict.py
+++ frak/compile_dict.py
@@ -1,9 +1,15 @@
 # Read through a json doc, adding lines to a txt dict with each card entry
+# Updated for set 2
 import json
 import os
 
 with open('cards.json') as json_data, open('cards.txt', 'w') as outfile:
-    wrapper = json.load(json_data)
-    card_list = wrapper['Cards']
+    #Initial call, previous json format
+    #wrapper = json.load(json_data)
+    #card_list = wrapper['Cards']
+
+    card_list = json.load(json_data)
     for card in card_list:
-        outfile.write(card['cardName']+'\n')
+        print card['Name']
+        encoded = card['Name'].encode('utf-8')
+        outfile.write(encoded+'\n')
